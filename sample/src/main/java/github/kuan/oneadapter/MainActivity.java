30864e412bbc2f993940b5e688d46d6a6bc63129
eekidu <wosyou@qq.com>
2020-12-12 23:28:47 +0800

    update

--- sample/src/main/java/github/kuan/oneadapter/MainActivity.java
+++ sample/src/main/java/github/kuan/oneadapter/MainActivity.java
@@ -1,18 +1,12 @@
 package github.kuan.oneadapter;
 
 import android.os.Bundle;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.chad.library.adapter.base.BaseQuickAdapter;
-import com.chad.library.adapter.base.listener.OnItemClickListener;
-import com.chad.library.adapter.base.viewholder.BaseViewHolder;
 
 import java.util.ArrayList;
 import java.util.List;
